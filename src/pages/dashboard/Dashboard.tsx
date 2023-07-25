7e12498dbfd29a77bd323c6a3dd2f9f4bed0bc3b
Cleidimar Dias <cleidimarcdsc@gmail.com>
2023-07-25 09:43:35 -0300

    instalado json-server

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,19 +1,39 @@
 
 
+import { Typography } from "@mui/material"
 import { FerramentasDeDetalhe } from "../../shared/components"
 import { LayoutBaseDePagina } from "../../shared/layouts"
 
-export const Dashboard: React.FC = () =>{
+export const Dashboard: React.FC = () => {
 
-    return(
+    return (
         <LayoutBaseDePagina titulo="Página Inicial"
-         barraDeFerramentas={<FerramentasDeDetalhe
-          mostrarBotaoSalvarEFechar
-          
-          />} >              
-            base
+            barraDeFerramentas={<FerramentasDeDetalhe
+                mostrarBotaoSalvarEFechar
+
+            />} >
+            <Typography>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia repudiandae quo harum sequi? Atque consectetur adipisci amet unde dolorem, id possimus, quae numquam, velit iusto dignissimos? Nisi nesciunt mollitia harum.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse voluptatem nemo maxime atque tempore, omnis molestiae fuga corrupti velit mollitia quo perspiciatis ratione deserunt corporis quasi tenetur eaque ipsa aspernatur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam quisquam, natus delectus, eius earum, possimus aperiam facere assumenda iste iure sequi eligendi nobis facilis. Vero rerum dolore deleniti odio laudantium.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam alias voluptatibus laudantium neque quod, hic quia magni atque quasi libero numquam possimus ratione consectetur nulla officiis sint? Et, aspernatur dolores?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, ab necessitatibus eum optio et tenetur corrupti ratione saepe nisi sapiente fugit aspernatur incidunt ipsam eos exercitationem facilis quis perspiciatis maiores.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde doloribus officiis a sunt. Debitis assumenda non delectus veritatis fugiat voluptate voluptates. Neque voluptate nam amet aut doloribus facilis dolores earum!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime soluta, dolorum, rem quos consequuntur sapiente, optio eaque repellat sint odio cumque a exercitationem officiis magni dicta at accusamus magnam deserunt!
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur earum similique repellat culpa commodi molestiae alias deleniti quam laudantium eveniet magni fugit, tempore doloremque fugiat sed veritatis veniam suscipit nam.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi maiores iste temporibus asperiores minima vitae ex saepe dignissimos laboriosam adipisci pariatur odit, blanditiis perspiciatis molestias, at veritatis, molestiae voluptatibus ut!
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam fugiat perspiciatis, nihil voluptate nobis, sunt sit accusamus deleniti harum distinctio inventore ad. Omnis odit minus rem ducimus, corporis tenetur. Accusantium?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio eius atque esse tenetur temporibus expedita error asperiores maxime voluptas accusamus, eveniet adipisci cupiditate, eos modi reprehenderit fuga vitae perferendis rerum.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa quae suscipit quam nemo nam quos, quidem, harum porro ratione accusamus aspernatur deserunt excepturi sequi at officia obcaecati, beatae cupiditate eligendi.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic non, quia illum nihil ut corporis officiis veritatis incidunt. Officiis autem, atque impedit eligendi nostrum dolorem natus perferendis recusandae similique quis?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat doloremque, rem saepe sequi quisquam beatae deleniti libero pariatur dicta id maxime minima facere perspiciatis exercitationem mollitia porro. Tempore, magni vero.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, quod error et molestiae quasi blanditiis, explicabo soluta possimus quibusdam libero velit ducimus reprehenderit ratione assumenda tenetur saepe laudantium labore voluptates!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam delectus eius laudantium unde fugit nihil modi. Eius consequuntur nulla, temporibus sunt perspiciatis quae labore? Ex inventore illum praesentium veritatis deleniti?
+
+            </Typography>
+
         </LayoutBaseDePagina>
 
-        
+
     )
 }
